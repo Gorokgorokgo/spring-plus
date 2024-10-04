680f9a3ab6f9392b54a6fceb9892c948692c77fe
Park_Yong_Jun <dydwnsl02@naver.com>
2024-10-04 19:02:38 +0900

    Level 1-1 : Todos 호출 에러 해결

--- src/main/java/org/example/expert/domain/todo/dto/request/TodoSaveRequest.java
+++ src/main/java/org/example/expert/domain/todo/dto/request/TodoSaveRequest.java
@@ -1,17 +1,34 @@
 package org.example.expert.domain.todo.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.expert.domain.common.entity.Timestamped;
+
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TodoSaveRequest {
+public class TodoSaveRequest extends Timestamped {
+
+    @NotBlank
+    private String contents;
 
     @NotBlank
     private String title;
+
+    @NotNull
+    @Positive
+    private Long userId;
+
     @NotBlank
-    private String contents;
+    private String weather;
+
+    private LocalDateTime created_at;
+
+    private LocalDateTime modified_at;
 }
