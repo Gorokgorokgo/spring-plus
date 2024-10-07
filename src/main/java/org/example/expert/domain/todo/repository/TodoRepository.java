04fcfeb29902a50220e1dd0480880d89f29a90d7
Park_Yong_Jun <dydwnsl02@naver.com>
2024-10-07 19:32:29 +0900

    Level 1-5 : JPQL을 사용하여 weather, from, to 조건으로 조회하기

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -7,15 +7,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
 
-    @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
-    Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
+  @Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
+  Page<Todo> findAllByOrderByModifiedAtDesc(Pageable pageable);
 
-    @Query("SELECT t FROM Todo t " +
-            "LEFT JOIN t.user " +
-            "WHERE t.id = :todoId")
-    Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+  @Query("SELECT t FROM Todo t " +
+      "LEFT JOIN t.user " +
+      "WHERE t.id = :todoId")
+  Optional<Todo> findByIdWithUser(@Param("todoId") Long todoId);
+
+
+  @Query("SELECT t FROM Todo t WHERE (:from IS NULL OR t.modifiedAt >= :from) " +
+      "AND (:to IS NULL OR t.modifiedAt <= :to) " +
+      "AND (:weather IS NULL OR t.weather = :weather) " +
+      "ORDER BY t.modifiedAt DESC")
+  Page<Todo> findtodos(LocalDateTime from, LocalDateTime to, String weather, Pageable pageable);
+
 }
